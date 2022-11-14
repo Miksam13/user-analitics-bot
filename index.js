6933a5759791f5c59a0a3d5f71b76d60f682b386
Miksam13 <74088676+Miksam13@users.noreply.github.com>
2022-11-14 21:04:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Telegraf } = require('telegraf');
 
+const users = ['Miksam_13', 'vad22', 'ju_dio', 'quartz555', 'Llairet'];
+
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -9,33 +11,83 @@ bot.start((ctx) => ctx.reply('Hello!'));
 const usersMessage = [];
 
 function messageCounterByUser(users) {
-    let result = {};
+    let resultActive = {};
     let message = '';
 
     users.forEach((a) => {
-        if (result[a] !== undefined) {
-            ++result[a];
+        if (resultActive[a] !== undefined) {
+            ++resultActive[a];
         }
         else {
-            result[a] = 1;
+            resultActive[a] = 1;
         }
     });
 
-    const sortable = Object.entries(result)
+    resultActive = Object.entries(resultActive)
       .sort(([,a],[,b]) => b-a)
       .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
-    console.log(sortable);
 
-    for (let key in sortable) {
+    for (const key in resultActive) {
+        message += `${key} - ${resultActive[key]} сообщения(е)\n`;
+    }
+    return message;
+}
+
+function messageByNotActiveUser(usersAll) {
+    const usersNotActive = usersAll.filter(el => !usersMessage.includes(el));
+    let resultNotActive = {};
+    let message = '';
+
+    usersNotActive.forEach((a) => {
+        ++resultNotActive[a];
+    });
+
+    for (const key in resultNotActive) {
+        message += `${key}, `;
+    }
+    return message + 'были не активными, ребята, давайте активничайте';
+}
+
+function messageAllActiveUser(usersAll) {
+    const usersNotActive = usersAll.filter(el => !usersMessage.includes(el));
+    let resultNotActiveAll = {};
+    let resultActiveAll = {};
+    let message = '';
+
+    usersNotActive.forEach((a) => {
+        resultNotActiveAll[a] = 0;
+    });
+
+    users.forEach((a) => {
+        if (resultActiveAll[a] !== undefined) {
+            ++resultActiveAll[a];
+        } else {
+            resultActiveAll[a] = 1;
+        }
+    });
+
+    let result = { ...resultActiveAll, ...resultNotActiveAll };
+
+    result = Object.entries(result)
+      .sort(([,a],[,b]) => b-a)
+      .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+    for (const key in result) {
         message += `${key} - ${result[key]} сообщения(е)\n`;
     }
     return message;
 }
 
 bot.hears('/getstat', (ctx) => {
-    bot.telegram.sendMessage(
-      ctx.chat.id,
-      messageCounterByUser(usersMessage));
+    ctx.reply(messageCounterByUser(usersMessage));
+})
+
+bot.hears('/getnotactive', (ctx) => {
+    ctx.reply(messageByNotActiveUser(users));
+})
+
+bot.hears('/getallactive', (ctx) => {
+    ctx.reply(messageAllActiveUser(users));
 })
 
 bot.on('message',  (ctx) => {
